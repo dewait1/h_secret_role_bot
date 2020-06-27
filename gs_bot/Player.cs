98ea22947bfa0f714d8ac439a887116878229f14
dewait1 <artem.samokhval@hotmail.com>
2020-06-27 13:42:45 +0200

    fixed some errors

---
+++ gs_bot/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gs_bot
+{
+    class Player
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int id { get; set; }
+        public string party { get; set; }
+        public string role { get; set; }
+    }
+}
